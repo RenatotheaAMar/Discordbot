c8c3364a2b2dc7fda347601a7a2d9f847d135ba5
RenatotheaAMar <amar.cakaj.5@gmail.com>
2025-07-15 19:37:34 +0200

    bot.js

--- bot.js
+++ bot.js
@@ -23,8 +23,8 @@ require('dotenv').config();
 const { google } = require('googleapis');
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: './google-service-account.json',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+  keyFile: 'google-service-account.json',
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
